fdb245d77b57daf1fe90251f536977c7af56614e
HemanthDas <hemanthchoudary4@gmail.com>
2023-10-29 15:36:35 +0530

    Added New Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import DashBoard from "./pages/dashboard";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <DashBoard />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const islogged = true;
+  return (
+    <div id="navbar">
+      <label>MED TO CONNECT</label>
+      <div className="links">
+        <a href="">link1</a>
+        <a href="">link2</a>
+        <a href="">link3</a>
+        {islogged ? <a href="">Login/Register</a> : <a href="">Logout</a>}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
